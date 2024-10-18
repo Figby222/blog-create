e82d8727a17fb88934f73f9fa37e86a3e0ad0309
Figby222 <figby222@outlook.com>
2024-10-18 04:26:55 -0700

    Add EditBlogPostForm Prop Types

--- src/components/EditBlogPostForm.jsx
+++ src/components/EditBlogPostForm.jsx
@@ -1,3 +1,14 @@
-const EditBlogPostForm = () => {};
+import PropTypes from "prop-types";
+
+const EditBlogPostForm = ({ useAllData, updateBlogPut }) => {
+    return (
+        <>
+        </>
+    )
+};
+EditBlogPostForm.propTypes = {
+    useAllData: PropTypes.func.isRequired,
+    updateBlogPut: PropTypes.func.isRequired
+};
 
 export default EditBlogPostForm;
