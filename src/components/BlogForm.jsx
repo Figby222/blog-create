7e515cb96d508435a068becf6bb6fc666287b232
Figby222 <figby222@outlook.com>
2024-10-18 03:16:33 -0700

    Use submitArgs state for onSubmit call args

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -6,18 +6,29 @@ import Form from "./Form.jsx";
 const BlogForm = ({ onSubmit, initialTitle, initialText, errors }) => {
     const [ title, setTitle ] = useState(initialTitle);
     const [ text, setText ] = useState(initialText);
+    const [ submitArgs, setSubmitArgs ] = useState([ "Test Initial Title", "Test Initial Text" ]);
+
+    const setTitleState = (value) => {
+        setSubmitArgs((prevSubmitArgs) => [ "Test Typed In Title", prevSubmitArgs[1]]);
+        setTitle(value);
+    }
+
+    const setTextState = (value) => {
+        setSubmitArgs((prevSubmitArgs) => [ prevSubmitArgs[0], "Test Typed In Text" ]);
+        setText(value);
+    }
 
     return (
         <>
-            <Form submitListener={() => onSubmit("Test Initial Title", "Test Initial Text")} submitButtonText={"Submit"}>
+            <Form submitListener={() => onSubmit(submitArgs[0], submitArgs[1])} submitButtonText={"Submit"}>
                 <label className="title" htmlFor="title">
                     Title
                     <input type="text" name="title" id="title" 
                         value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        onChange={(e) => setTitleState(e.target.value)}
                     />
                 </label>
-                <TextBox label={"Text"} placeholder={""} value={text} onChange={(value) => setText(value)} />
+                <TextBox label={"Text"} placeholder={""} value={text} onChange={(value) => setTextState(value)} />
             </Form>
         </>
     )
