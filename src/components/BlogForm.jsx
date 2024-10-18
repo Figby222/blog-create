622bf042a9cef852c548e5b4e79030a571508bcc
Figby222 <figby222@outlook.com>
2024-10-17 21:16:42 -0700

    Add BlogPost Prop Types

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,3 +1,19 @@
-const BlogForm = () => {};
+import PropTypes from "prop-types";
+
+const BlogForm = ({ onSubmit, initialTitle, initialText, errors }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+BlogForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    initialTitle: PropTypes.string,
+    initialText: PropTypes.string,
+    errors: PropTypes.arrayOf(PropTypes.shape({
+        message: PropTypes.string.isRequired
+    }))
+}
 
 export default BlogForm;
