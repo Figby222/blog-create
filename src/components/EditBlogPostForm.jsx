b9bddcdc7074f3f602807776878493c7389c4b64
Figby222 <figby222@outlook.com>
2024-10-18 04:43:49 -0700

    Only render loading if loading

--- src/components/EditBlogPostForm.jsx
+++ src/components/EditBlogPostForm.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 const EditBlogPostForm = ({ useAllData, updateBlogPut }) => {
     const { error, loading, data} = useAllData();
 
+    if (loading) {
+        return <h1 className="loading">Loading</h1>
+    }
+
     return (
         <>
-            <h1 className="loading">Loading</h1>
         </>
     )
 };
