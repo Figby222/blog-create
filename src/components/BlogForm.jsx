ae2233989f5f413541ee3bb369f246f168baa644
Figby222 <figby222@outlook.com>
2024-10-17 23:04:55 -0700

    Use text state for text input value

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -4,16 +4,18 @@ import { useState } from "react";
 
 const BlogForm = ({ onSubmit, initialTitle, initialText, errors }) => {
     const [ title, setTitle ] = useState(initialTitle);
+    const [ text, setText ] = useState(initialText);
+
     return (
         <>
             <label className="title" htmlFor="title">
                 Title
                 <input type="text" name="title" id="title" 
                     value={title}
-                    onChange={(e) => setTitle("Test Typed In Title")}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
             </label>
-            <TextBox label={"Text"} placeholder={""} value={initialText} onChange={() => {}} />
+            <TextBox label={"Text"} placeholder={""} value={text} onChange={(value) => setText("Test Typed In Text")} />
         </>
     )
 };
