aab380ed754db194a4eec62f1faf82bdf198bb17
Figby222 <figby222@outlook.com>
2024-10-17 22:54:20 -0700

    Use title state for title input value

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import TextBox from "./TextBox.jsx";
+import { useState } from "react";
 
 const BlogForm = ({ onSubmit, initialTitle, initialText, errors }) => {
+    const [ title, setTitle ] = useState(initialTitle);
     return (
         <>
             <label className="title" htmlFor="title">
                 Title
-                <input type="text" name="title" id="title" value={initialTitle} />
+                <input type="text" name="title" id="title" 
+                    value={title}
+                    onChange={(e) => setTitle("Test Typed In Title")}
+                />
             </label>
             <TextBox label={"Text"} placeholder={""} value={initialText} onChange={() => {}} />
         </>
