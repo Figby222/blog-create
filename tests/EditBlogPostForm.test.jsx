42bd35af0f29d2c893a7c6938d336acfaec06999
Figby222 <figby222@outlook.com>
2024-10-19 01:20:14 -0700

    Make sure text can have different initial value

--- tests/EditBlogPostForm.test.jsx
+++ tests/EditBlogPostForm.test.jsx
@@ -167,4 +167,20 @@ describe("Text", () => {
         expect(textInput.value)
             .toMatch(/Test Text/i);
     })
+
+    it("Has different provided initial value", () => {
+        const mockUseAllData = getUseAllDataMock(false, false, {
+            title: "Test Different Title",
+            text: "Test Different Text"
+        });
+
+        render(<EditBlogPostForm useAllData={mockUseAllData} updateBlogPut={() => {}} />);
+
+        const textInput = screen.queryByLabelText(/Text/i);
+
+        expect(textInput.value)
+            .not.toMatch(/Test Text/i)
+        expect(textInput.value)
+            .toMatch(/Test Different Text/i);
+    })
 })
