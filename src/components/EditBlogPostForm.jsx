c2295655a7cb38d6585d0c1c4bcfc63c9d866cb6
Figby222 <figby222@outlook.com>
2024-10-19 02:13:14 -0700

    Call updateBlogPut()

--- src/components/EditBlogPostForm.jsx
+++ src/components/EditBlogPostForm.jsx
@@ -14,6 +14,7 @@ const EditBlogPostForm = ({ useAllData, updateBlogPut }) => {
         return <h1 className="error">An error has occurred</h1>
     }
 
+    updateBlogPut();
     return (
         <>
             <BlogForm onSubmit={() => {}} initialTitle={data.title} initialText={data.text} errors={[]} />
