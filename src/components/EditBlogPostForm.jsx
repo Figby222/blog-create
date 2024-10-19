70db491e7414d6e73a7ff04913d8741b009760e1
Figby222 <figby222@outlook.com>
2024-10-19 02:44:12 -0700

    Call updateBlogPut() with data title & text

--- src/components/EditBlogPostForm.jsx
+++ src/components/EditBlogPostForm.jsx
@@ -17,7 +17,7 @@ const EditBlogPostForm = ({ useAllData, updateBlogPut }) => {
 
     return (
         <>
-            <BlogForm onSubmit={() => updateBlogPut("Test Title", "Test Text")} initialTitle={data.title} initialText={data.text} errors={[]} />
+            <BlogForm onSubmit={() => updateBlogPut(data.title, data.text)} initialTitle={data.title} initialText={data.text} errors={[]} />
         </>
     )
 };
