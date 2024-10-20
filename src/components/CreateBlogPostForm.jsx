9133f7c63a6c4c7a39827b2e622951250d48b593
Figby222 <figby222@outlook.com>
2024-10-19 23:22:10 -0700

    Nest fields in Form

--- src/components/CreateBlogPostForm.jsx
+++ src/components/CreateBlogPostForm.jsx
@@ -1,14 +1,18 @@
 import PropTypes from "prop-types";
 import TextBox from "./TextBox.jsx";
+import Form from "./Form.jsx";
 
 const CreateBlogPostForm = ({ createBlogPost }) => {
     return (
         <>
-            <label className="title" htmlFor="title">
-                Title
-                <input type="text" name="title" id="title" />
-            </label>
-            <TextBox label={"Text"} placeholder={""} value={""} onChange={() => {}} />
+            <Form submitListener={() => {}} submitButtonText={""}>
+                <label className="title" htmlFor="title">
+                    Title
+                    <input type="text" name="title" id="title" />
+                </label>
+                <TextBox label={"Text"} placeholder={""} value={""} onChange={() => {}} />
+
+            </Form>
         </>
     )
 };
