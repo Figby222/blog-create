ccb80efe52e31310daac0e19ecae9af1fbcd5037
Figby222 <figby222@outlook.com>
2024-10-19 22:57:01 -0700

    Add CreateBlogPostForm Prop Types

--- src/components/CreateBlogPostForm.jsx
+++ src/components/CreateBlogPostForm.jsx
@@ -1,3 +1,14 @@
-const CreateBlogPostForm = () => {};
+import PropTypes from "prop-types";
+
+const CreateBlogPostForm = ({ createBlogPost }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+CreateBlogPostForm.propTypes = {
+    createBlogPost: PropTypes.func.isRequired
+};
 
 export default CreateBlogPostForm;
