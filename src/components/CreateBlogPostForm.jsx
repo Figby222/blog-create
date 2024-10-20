8112255f6843e3a738679453049eff89ff38ee32
Figby222 <figby222@outlook.com>
2024-10-19 23:14:53 -0700

    Make Text an input

--- src/components/CreateBlogPostForm.jsx
+++ src/components/CreateBlogPostForm.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import TextBox from "./TextBox.jsx";
 
 const CreateBlogPostForm = ({ createBlogPost }) => {
     return (
@@ -7,7 +8,7 @@ const CreateBlogPostForm = ({ createBlogPost }) => {
                 Title
                 <input type="text" name="title" id="title" />
             </label>
-            <p className="text">Text</p>
+            <TextBox label={"Text"} placeholder={""} value={""} onChange={() => {}} />
         </>
     )
 };
