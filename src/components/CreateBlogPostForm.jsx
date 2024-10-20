ded5ed1f19dbc34f4dc5ab771166b8b69c41be9a
Figby222 <figby222@outlook.com>
2024-10-19 23:57:06 -0700

    Merge elements into BlogForm component

--- src/components/CreateBlogPostForm.jsx
+++ src/components/CreateBlogPostForm.jsx
@@ -1,24 +1,19 @@
 import PropTypes from "prop-types";
-import TextBox from "./TextBox.jsx";
-import Form from "./Form.jsx";
+import BlogForm from "./BlogForm.jsx";
+
 
 const CreateBlogPostForm = ({ createBlogPost }) => {
-
     return (
         <>
-            <Form submitListener={() => createBlogPost("Test Title", "Test Text")} submitButtonText={"Submit"}>
-                <label className="title" htmlFor="title">
-                    Title
-                    <input type="text" name="title" id="title" />
-                </label>
-                <TextBox label={"Text"} placeholder={""} value={""} onChange={() => {}} />
-            </Form>
+            <BlogForm onSubmit={(title, text) => createBlogPost(title, text)} initialTitle={""} initialText={""} errors={[]} />
         </>
     )
 };
 
+
 CreateBlogPostForm.propTypes = {
     createBlogPost: PropTypes.func.isRequired
 };
+
 
 export default CreateBlogPostForm;
