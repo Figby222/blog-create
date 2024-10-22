84b1d56eb5c6d6fe96cc05798feade8ae61d565e
Figby222 <figby222@outlook.com>
2024-10-22 03:56:03 -0700

    Add /posts/create route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
 import App from './App';
 import EditBlogPostForm from "./components/EditBlogPostForm.jsx";
-import { useBlogPostData, updateBlogPut } from "./utils/api.jsx";
+import CreateBlogPostForm from "./components/CreateBlogPostForm.jsx";
+import { useBlogPostData, updateBlogPut, createBlogPost } from "./utils/api.jsx";
+
 
 const routes = [
     {
@@ -10,7 +12,13 @@ const routes = [
     {
         path: "/posts/:postId/edit",
         element: <EditBlogPostForm  useAllData={useBlogPostData} updateBlogPut={updateBlogPut} />
+    },
+    {
+        path: "/posts/create",
+        element: <CreateBlogPostForm createBlogPost={createBlogPost} />
     }
 ];
+
+
 
 export default routes;
