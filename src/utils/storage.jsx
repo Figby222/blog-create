054fef9f3a2533d28021d5f56d961d6daa35cbd3
Figby222 <figby222@outlook.com>
2024-10-23 22:37:48 -0700

    Add storage.jsx

---
+++ src/utils/storage.jsx
@@ -0,0 +1,12 @@
+const storeBearerToken = (token) => {
+    localStorage.setItem("bearerToken", token);
+    console.log(getBearerToken());
+
+    console.log("A");
+}
+
+const getBearerToken = () => {
+    return localStorage.getItem("bearerToken");
+}
+
+export { storeBearerToken };
