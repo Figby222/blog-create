be0cafdc4a34d61b547871a1420ab5b086dacb81
Figby222 <figby222@outlook.com>
2024-10-25 05:52:54 -0700

    Set publish input value to initialPublishedStatus

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -21,7 +21,7 @@ const BlogForm = ({ onSubmit, initialTitle, initialText, initialPublishedStatus,
                 </label>
                 <label className="publish-status" htmlFor="publish-status">
                     Publish
-                    <input type="checkbox" name="publish_status" id="publish-status" checked />
+                    <input type="checkbox" name="publish_status" id="publish-status" checked={initialPublishedStatus} />
                 </label>
                 <TextBox label={"Text"} placeholder={""} value={text} onChange={(value) => setText(value)} />
             </Form>
