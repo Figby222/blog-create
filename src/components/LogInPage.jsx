15b06412996c1b92390b4fd20b16868522bc1227
Figby222 <figby222@outlook.com>
2024-10-26 01:03:29 -0700

    Add LogInPage.jsx

---
+++ src/components/LogInPage.jsx
@@ -0,0 +1,58 @@
+import PropTypes from "prop-types";
+import Form from "./Form";
+import { useState } from "react";
+import Errors from "./Errors.jsx";
+
+const LogInPage = ({ logInUser, storeBearerToken }) => {
+    const [ username, setUsername ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ errors, setErrors ] = useState([]);
+
+    const submitHandler = async (username, email, password) => {
+        const response = await logInUser(username, email, password);
+
+        response.errors && setErrors(response.errors);
+
+        console.log(response);
+
+        response.token && storeBearerToken(response.token);
+    }
+
+    return (
+        <>
+            <Form submitListener={() => submitHandler(username, email, password)} submitButtonText={"Submit"}>
+                <section className="errors">
+                    <Errors errors={errors} />
+                </section>
+                <label className="username" htmlFor="username">
+                    Username
+                    <input type="text" name="username" id="username" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label className="email" htmlFor="email">
+                    Email
+                    <input type="text" name="email" id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <label className="password" htmlFor="password">
+                    Password
+                    <input type="password" name="password" id="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+            </Form>
+        </>
+    )
+};
+LogInPage.propTypes = {
+    logInUser: PropTypes.func.isRequired,
+    storeBearerToken: PropTypes.func.isRequired,
+};
+
+export default LogInPage;
