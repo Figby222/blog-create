92c141846aa3687ab580e39126d3168d8d4448bc
Figby222 <figby222@outlook.com>
2024-10-29 03:43:45 -0700

    Add ErrorPage errorElement to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,31 +6,38 @@ import LogInPage from "./components/LogInPage.jsx";
 import Blogs from "./components/Blogs.jsx";
 import { useBlogPostData, updateBlogPut, createBlogPost, deletePost, createAnAccount, logInUser, deleteComment, useBlogsListData } from "./utils/api.jsx";
 import { getBearerToken, storeBearerToken } from "./utils/storage.jsx"
+import ErrorPage from "./components/ErrorPage.jsx";
 
 const routes = [
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />
     },
     {
         path: "/posts/:postId/edit",
-        element: <EditBlogPostForm  useAllData={useBlogPostData} updateBlogPut={updateBlogPut} getBearerToken={getBearerToken} deletePost={deletePost} deleteComment={deleteComment} />
+        element: <EditBlogPostForm  useAllData={useBlogPostData} updateBlogPut={updateBlogPut} getBearerToken={getBearerToken} deletePost={deletePost} deleteComment={deleteComment} />,
+        errorElement: <ErrorPage />
     },
     {
         path: "/posts/create",
-        element: <CreateBlogPostForm createBlogPost={createBlogPost} getBearerToken={getBearerToken} />
+        element: <CreateBlogPostForm createBlogPost={createBlogPost} getBearerToken={getBearerToken} />,
+        errorElement: <ErrorPage />
     },
     {
         path: "/sign-up",
-        element: <SignUpPage createAnAccount={createAnAccount} />
+        element: <SignUpPage createAnAccount={createAnAccount} />,
+        errorELement: <ErrorPage />
     },
     {
         path: "/log-in",
-        element: <LogInPage storeBearerToken={storeBearerToken} logInUser={logInUser} />
+        element: <LogInPage storeBearerToken={storeBearerToken} logInUser={logInUser} />,
+        errorElement: <ErrorPage />
     },
     {
         path: "/posts",
-        element: <Blogs useAllData={useBlogsListData} getBearerToken={getBearerToken} />
+        element: <Blogs useAllData={useBlogsListData} getBearerToken={getBearerToken} />,
+        errorElement: <ErrorPage />
     }
 
 
