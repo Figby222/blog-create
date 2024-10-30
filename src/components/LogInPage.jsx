e9a1ce673f62b16b0a983d9be8a6775ff364ceaf
Figby222 <figby222@outlook.com>
2024-10-30 03:19:22 -0700

    Import LogInPage.css

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Errors from "./Errors.jsx";
 import { Link, useNavigate } from "react-router-dom"
 import Header from "./Header.jsx";
+import "../styles/LogInPage.css";
 
 const LogInPage = ({ logInUser, storeBearerToken }) => {
     const [ username, setUsername ] = useState("");
